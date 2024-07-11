a2a93ed6187d0a915433226c041df721a88d5723
Amos Oluoch <aoluoch271@gmail.com>
2024-07-11 13:10:33 +0300

    arr

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 // Write your solution here!
+// Initial array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Function to reset the cats array before each test
+beforeEach(function () {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
 function destructivelyPrependCat(name) {
     cats.unshift(name);
-  }
-  
-  function destructivelyRemoveLastCat() {
+}
+
+function destructivelyRemoveLastCat() {
     cats.pop();
-  }
+}
 
-  function destructivelyRemoveFirstCat() {
+function destructivelyRemoveFirstCat() {
     cats.shift();
-  }
-  
-  function appendCat(name) {
+}
+
+function appendCat(name) {
     return [...cats, name];
-  }
-  
-  function prependCat(name) {
+}
+
+function prependCat(name) {
     return [name, ...cats];
-  }
+}
 
-  function removeLastCat() {
+function removeLastCat() {
     return cats.slice(0, cats.length - 1);
-  }
+}
 
-  function removeFirstCat() {
+function removeFirstCat() {
     return cats.slice(1);
-  }
+}
+
   
   
   
